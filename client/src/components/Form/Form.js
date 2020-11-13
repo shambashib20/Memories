ebf819add84f32d04b7b407854aa2e5069618586
SHAMBASHIB MAJUMDAR <shambashibmajumdar2000@gmail.com>
2020-11-13 11:24:36 +0530

    1st Commit

---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,26 @@
+import React ,{ useSate } from 'react';
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
+
+import useStyles from './styles';
+
+                
+const Form = () => {
+    const [ postData, setPostData ] = useState({
+        
+    });
+
+    const classes = useStyles();
+    const handleSubmit = () => {
+
+    }
+    return (
+        <Paper className={classes.paper}>
+            <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
+            <Typography variant="h6">Creating a Memory</Typography>
+            <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={}/>
+            </form>
+        </Paper>
+    );
+}
+
+export default Form;
