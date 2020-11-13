d12c130ebb8160c00cd7e76427f6cea6bd85efce
SHAMBASHIB MAJUMDAR <shambashibmajumdar2000@gmail.com>
2020-11-13 14:36:23 +0530

    changes done

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -15,7 +15,10 @@ const Form = () => {
         <Paper className={classes.paper}>
             <form autoComplete="off" noValidate className={classes.form} onSubmit={handleSubmit}>
             <Typography variant="h6">Creating a Memory</Typography>
-            <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={}/>
+            <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })}/>
+            <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })}/>
+            <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })}/>
+            <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })}/>
             </form>
         </Paper>
     );
